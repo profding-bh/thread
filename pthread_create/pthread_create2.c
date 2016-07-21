8937b80fb522896a3ba65d4b531e0ed4b19ea315
Eddyding <920398694@qq.com>
2016-07-21 15:28:35 +0800

    修改pthread2

--- pthread_create/pthread_create2.c
+++ pthread_create/pthread_create2.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
-
+#include<string.h>
+#include<errno.h>
 
 pthread_t ntid;
 
@@ -30,14 +31,31 @@ void *thr_fn(void *arg){
 
 int main(){
 
+extern    int errno;
+    errno = 0;
+    errno = pthread_create(&ntid,NULL,thr_fn,NULL);
+    if(0 != errno){
+         printf("can't create thread:%s \n",strerror(errno));
+    }
+/*
     int err;
     err = pthread_create(&ntid,NULL,thr_fn,NULL);
     if(0 != err){
-        char *msg = strerror(err);
-         printf("can't create thread:%s \n",msg);
+         printf("can't create thread:%s \n",strerror(err));
     }
-
+    */
     printids("main thread:");
-    sleep(1);
+     sleep(1);
+
+     /*
+      *
+      * 为什么需要加sleep(1)?
+      *
+      * 首先这段代码需要处理主线程和子线程之间的竞争问题。
+      * 首先是主线程必须休眠，如果主线程不休眠其就可能退出。
+      * 这样新线程还没有运行，这个进程就可能已经终止了。如果你
+      * 注释了sleep(1);发现整个进程直接退出。
+      *
+      * */
     return 0;
 }
